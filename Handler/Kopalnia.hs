35c5607a09233d1c226aff854a7f3e0e16016fc7
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2015-06-06 22:35:38 -0400

    Added making sure that the title is not empty in postKopalniaEditTytulR

--- Handler/Kopalnia.hs
+++ Handler/Kopalnia.hs
@@ -23,6 +23,7 @@ module Handler.Kopalnia (
 
 import Import
 import Enums
+import qualified Data.Text as T
 import Text.Read (reads)
 -- import Network.HTTP.Types (status200)
 -- import Network.Wai        (responseLBS)
@@ -110,8 +111,10 @@ maybeRead _ = Nothing
 -- This function is quite ugly at the moment, but at least it works.
 -- TODO: It should be improved by using a monad combinator such as EitherT.
 --       Example: http://stackoverflow.com/questions/13252889/elegant-haskell-case-error-handling-in-sequential-monads
--- The first argument is a validation/conversion function that returns Either <error message> <converted value>.
--- If everything succeeds then this value will be passed to the second argument alongside the db lookup criterion.
+-- The first argument is a validation/conversion function that takes the raw (but trimmed) value of type Text
+-- and returns Either <error message> <converted value>.
+-- If everything succeeds then this value will be passed to the second argument alongside the db lookup criterion,
+-- typically applied against updateWhere.
 processXEditable :: (Text -> Either Text a) -> (Filter Kopalnia -> a -> Handler ()) -> Handler Text
 processXEditable vald upd = do
     mLookupId <- lookupPostParam "pk"
@@ -121,7 +124,7 @@ processXEditable vald upd = do
             mValueRaw <- lookupPostParam "value"
             case mValueRaw of
                 Just valueRaw -> do
-                    eValue <- return $ vald valueRaw
+                    eValue <- return $ vald $ T.strip valueRaw
                     case eValue of
                         Right value -> do
                             let criterion = KopalniaLookupId ==. lookupId
@@ -137,7 +140,8 @@ processXEditable vald upd = do
 
 postKopalniaEditTytulR :: Handler Text
 postKopalniaEditTytulR = processXEditable vald upd where
-    vald = Right
+    vald v | T.length v > 0 = Right v
+           | otherwise = Left "Tytuł nie może być pusty"
     upd criterion value = runDB $ updateWhere [criterion] [KopalniaTytul =. value]
 
 postKopalniaEditRodzajR :: Handler Text
