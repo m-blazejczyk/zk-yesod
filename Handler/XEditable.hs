4be845787ebb9066238230cadf2829223984f6cd
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2015-10-03 23:06:41 -0400

    Updated comments

--- Handler/XEditable.hs
+++ Handler/XEditable.hs
@@ -16,7 +16,8 @@ data XEdVal = XEdNone                       -- No "value*" parameter was found a
     deriving (Show)
 
 -- This is a version of processXEditable for POST requests requiring a single parameter value.
--- The upd function in this case should simply return a value that will be passed to a call to runDb $ updateWhere.
+-- The upd function in this case should simply return a value that will be passed to a call
+--   to runDb $ updateWhere.
 processXEditable1 :: (Text -> Handler (Result a))
                   -> (a -> [Update Kopalnia])
                   -> Handler Text
@@ -28,17 +29,12 @@ processXEditable1 vald upd = processXEditable vald' upd'
             runDB $ updateWhere [criterion] (upd vals)
             return $ Success "OK"
 
--- This function processes an Ajax POST request coming from X-Editable.  Each such request should first be validated,
---   and then one or more database fields should be updated.
--- The first argument is a validation/conversion function that takes a list of the raw (but trimmed) values of type Text
---   and returns Result <converted value> in the Handler monad (to allow for database validation lookups).
---   The value array is ordered in the same way as the third argument to processXEditable.
---   The point of passing an entire array is to allow for validation across parameters.
--- If everything succeeds then the second argument function (upd) will be called to store the values returned by vald
---   in the database.
--- The last argument is an array of expected POST parameter names, or an empty array if only a single value is expected.
--- TODO: This function should be improved by using a monad combinator such as EitherT.
---       Example: http://stackoverflow.com/questions/13252889/elegant-haskell-case-error-handling-in-sequential-monads
+-- This function processes an Ajax POST request coming from X-Editable.  Each such request should
+--   first be validated, and then one or more database fields should be updated.
+-- The first argument is a validation/conversion function that takes an XEdVal and returns
+--   Result <converted value> in the Handler monad (to allow for database validation lookups).
+-- If everything succeeds then the second argument function (upd) will be called to store the values
+--   returned by vald in the database.
 processXEditable :: (XEdVal -> Handler (Result a))
                  -> (Filter Kopalnia -> a -> Handler (Result Text))
                  -> Handler Text
