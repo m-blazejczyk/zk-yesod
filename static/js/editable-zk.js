f5de7894aed6741de671abe2eba46ec2322aee36
Michal Blazejczyk <michal.blazejczyk@gmail.com>
2015-11-12 23:19:15 -0500

    Cleanup + multi-value handling

--- static/js/editable-zk.js
+++ static/js/editable-zk.js
@@ -2,19 +2,17 @@
 
     $.fn.editableZK = function(options) {
 
-        var field = this.attr('id');
+        var field = this.attr('id') || 'dummy';
         var modalId = field + '-modal';
 
         if ($('#' + modalId).length == 0) {
           // Merge with options from attributes.
           var settings = $.extend({
               type: this.attr('data-type'),
-              pk: this.attr('data-pk'),                         // primary key in the database
-              url: this.attr('data-url'),                       // url to send the POST request to
-              title: this.attr('data-title') || '',             // title of the popup (will default to 'name')
-              name: this.attr('data-name') || '',               // name of the field (if single input)
-              value: this.attr('data-value'),                   // raw value (if single input)
-              placeholder: this.attr('data-placeholder') || ''  // placeholder (if single input; will default to 'title')
+              pk: this.attr('data-pk'),          // primary key in the database
+              url: this.attr('data-url'),        // url to send the POST request to
+              title: this.attr('data-title'),    // title of the popup (will default to 'id')
+              value: this.attr('data-value')     // raw value (if single input)
           }, options);
 
           var formatInput = function(type, id, value, settings) {
@@ -32,12 +30,8 @@
 
           // TODO: Sanitize inputs!
 
-          if (settings.name == '' && settings.fields === undefined)
-            settings.name = 'dummy';
-          if (settings.title == '')
-            settings.title = settings.name;
-          if (settings.placeholder == '')
-            settings.placeholder = settings.title;
+          if (!settings.title)
+            settings.title = field;
 
           this.addClass('editable editable-click');
           this.attr('data-toggle', 'modal');
@@ -61,15 +55,15 @@
           htmlArr.push('      <div class="modal-body">');
           htmlArr.push('        <div class="row">');
 
-          if (settings.fields === undefined) {
+          if (!settings.fields) {
             // No fields definition - create a simple editor
             htmlArr.push('          <div class="col-md-12">');
-            htmlArr.push('            ' + formatInput(settings.type, modalId + settings.name, settings.value, settings));
+            htmlArr.push('            ' + formatInput(settings.type, modalId + '-edit', settings.value, settings));
             htmlArr.push('          </div>');
           } else {
             // Fields definition present - create an editor with labels
             for (var key in settings.fields) {
-              var fieldId = modalId + key;
+              var fieldId = modalId + '-' + key;
 
               if (settings.fields[key].placeholder === undefined)
                 settings.fields[key].placeholder = '';
@@ -80,7 +74,7 @@
               htmlArr.push('          <div class="col-md-8">');
               htmlArr.push('            ' + formatInput(settings.fields[key].type || settings.type,
                                                         fieldId,
-                                                        settings.fields[key].value || settings.value,
+                                                        typeof settings.value[key] == 'string' ? settings.value[key] : settings.value,
                                                         settings.fields[key]));
               htmlArr.push('          </div>');
             }
